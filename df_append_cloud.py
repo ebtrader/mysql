329655a599c6f8e8bfbcffd228ddbe5984169c52
ebtrader <jsiddique@gmail.com>
2023-04-11 10:57:11 -0400

    Commit

---
+++ df_append_cloud.py
@@ -0,0 +1,40 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy import text
+import pandas as pd
+# https://stackoverflow.com/questions/75273632/got-multiple-values-for-argument-schema
+# https://planetscale.com/blog/using-mysql-with-sql-alchemy-hands-on-examples
+
+hostname_file = 'host.txt'
+with open(hostname_file) as g:
+  hostname = g.read()
+
+# db_file = os.path.join (data_folder, 'db.txt')
+db_file = 'db.txt'
+with open(db_file) as f:
+  dbname = f.read()
+
+# username_file = os.path.join (data_folder, 'username.txt')
+username_file = 'username.txt'
+with open(username_file) as h:
+  username = h.read()
+
+# pwd_file = os.path.join (data_folder, 'pwd.txt')
+pwd_file = 'pwd.txt'
+with open(pwd_file) as i:
+  pwd = i.read()
+
+connection_string = 'mysql+mysqlconnector://' + username + ':' + pwd + '@' + hostname + '/' + dbname
+engine = create_engine(connection_string)
+
+# with engine.connect() as connection:
+#   connection.execute(text("CREATE TABLE sample (name VARCHAR(50), address VARCHAR(50))"))
+
+df = pd.read_csv('address_book.csv')
+# print(df)
+
+# conn = engine.connect()
+# df.to_sql('address_table', con=engine) # creates table from df
+
+df.to_sql('address_table', con=engine, if_exists='append') # appends table
+
